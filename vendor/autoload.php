2141385c9e3bc1bd05be0a315ebc3a4b972492c4
The Kabal <thekabal@gmail.com>
2014-09-27 21:47:36 -0600

    Update to the latest version of Smarty and SwiftMailer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit5ee2304c0284507f0a74306719aa17bb::getLoader();
+return ComposerAutoloaderInit85dd975e3486237e0f87f033be6487ec::getLoader();
