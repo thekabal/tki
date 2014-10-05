5d6c2a2fece23f1c9af133d7c3e64225e08566b4
The Kabal <thekabal@gmail.com>
2014-10-05 08:58:08 -0600

    EU Cookie law is no longer an issue, simplify code as a result. http://ico.org.uk/for_organisations/privacy_and_electronic_communications/the_guide/cookies

--- languages/english.ini.php
+++ languages/english.ini.php
@@ -767,7 +767,7 @@ l_tki                                   = "The Kabal Invasion";
 l_navigation                            = "Navigation";
 l_welcome_tki                           = "Welcome to The Kabal Invasion (TKI)!";
 l_tki_description                       = "TKI is a web-based massively multiplayer space combat and trading game inspired by the popular BBS games of TradeWars and Ultimate Universe.";
-l_cookie_warning                        = "The Kabal Invasion requires cookies. By signing up or logging in you agree to let us set cookies.";
+l_cookie_warning                        = "The Kabal Invasion has set a cookie for you. TKI requires cookies to function properly.";
 l_new_player                            = "New Player";
 
 [log]
