3068939b9cbfe0a2ee779f1c9e16003787e0c515
The Kabal <thekabal@gmail.com>
2014-11-09 07:00:09 -0700

    Eliminate global variables

--- classes/Team.php
+++ classes/Team.php
@@ -107,10 +107,8 @@ class Team
     }
 
     // Rewritten display of teams list
-    public static function displayAllTeams($db, $langvars, \Tki\Reg $tkireg)
+    public static function displayAllTeams($db, $langvars, \Tki\Reg $tkireg, $order, $type)
     {
-        global $color, $order, $type;
-
         echo "<br><br>" . $langvars['l_team_galax'] . "<br>";
         echo "<table style='width:100%; border:#fff 1px solid;' border='0' cellspacing='0' cellpadding='2'>";
         echo "<tr bgcolor=\"$tkireg->color_header\">";
