db77a531d704e0f23252df7aa79a28f511767010
The Kabal <thekabal@gmail.com>
2014-11-22 18:09:22 -0700

    Updates for password-compat and Smarty

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,8 +6,8 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    $vendorDir . '/ircmaxell/password-compat/lib/password.php',
     $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
     $vendorDir . '/adodb/adodb-php/adodb.inc.php',
     $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
+    $vendorDir . '/ircmaxell/password-compat/lib/password.php',
 );
