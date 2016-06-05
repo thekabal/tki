52505fd4da584516aa920174669e0ea671025264
Patrick <thekabal@gmail.com>
2016-06-04 22:22:59 -0600

    Update composer, and all dependencies to latest upstream

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,33 +23,48 @@ class ComposerAutoloaderInita544dbd9d1e6088f7541c342d08c3f73
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInita544dbd9d1e6088f7541c342d08c3f73', 'loadClassLoader'));
 
-        $map = require __DIR__ . '/autoload_namespaces.php';
-        foreach ($map as $namespace => $path) {
-            $loader->set($namespace, $path);
-        }
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        if ($useStaticLoader) {
+            require_once __DIR__ . '/autoload_static.php';
 
-        $map = require __DIR__ . '/autoload_psr4.php';
-        foreach ($map as $namespace => $path) {
-            $loader->setPsr4($namespace, $path);
-        }
+            call_user_func(\Composer\Autoload\ComposerStaticInita544dbd9d1e6088f7541c342d08c3f73::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
 
-        $classMap = require __DIR__ . '/autoload_classmap.php';
-        if ($classMap) {
-            $loader->addClassMap($classMap);
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
         }
 
         $loader->register(true);
 
-        $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $file) {
-            composerRequirea544dbd9d1e6088f7541c342d08c3f73($file);
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInita544dbd9d1e6088f7541c342d08c3f73::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirea544dbd9d1e6088f7541c342d08c3f73($fileIdentifier, $file);
         }
 
         return $loader;
     }
 }
 
-function composerRequirea544dbd9d1e6088f7541c342d08c3f73($file)
+function composerRequirea544dbd9d1e6088f7541c342d08c3f73($fileIdentifier, $file)
 {
-    require $file;
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+    }
 }
--- vendor/swiftmailer/swiftmailer/lib/classes/Swift/Plugins/ThrottlerPlugin.php
+++ vendor/swiftmailer/swiftmailer/lib/classes/Swift/Plugins/ThrottlerPlugin.php
@@ -11,7 +11,7 @@
 /**
  * Throttles the rate at which emails are sent.
  *
- * @author     Chris Corbyn
+ * @author Chris Corbyn
  */
 class Swift_Plugins_ThrottlerPlugin extends Swift_Plugins_BandwidthMonitorPlugin implements Swift_Plugins_Sleeper, Swift_Plugins_Timer
 {
@@ -131,7 +131,7 @@ class Swift_Plugins_ThrottlerPlugin extends Swift_Plugins_BandwidthMonitorPlugin
     /**
      * Sleep for $seconds.
      *
-     * @param int     $seconds
+     * @param int $seconds
      */
     public function sleep($seconds)
     {
@@ -151,15 +151,15 @@ class Swift_Plugins_ThrottlerPlugin extends Swift_Plugins_BandwidthMonitorPlugin
     {
         if (isset($this->_timer)) {
             return $this->_timer->getTimestamp();
-        } else {
-            return time();
         }
+
+        return time();
     }
 
     /**
      * Get a number of seconds to sleep for.
      *
-     * @param int     $timePassed
+     * @param int $timePassed
      *
      * @return int
      */
@@ -187,7 +187,7 @@ class Swift_Plugins_ThrottlerPlugin extends Swift_Plugins_BandwidthMonitorPlugin
     /**
      * Get a number of seconds to sleep for.
      *
-     * @param int     $timePassed
+     * @param int $timePassed
      *
      * @return int
      */
