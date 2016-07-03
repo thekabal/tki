5afe2530e93e1429e2d59350fa73f6d559ec5cdc
Patrick <thekabal@gmail.com>
2016-07-03 09:49:32 -0600

    Adodb -> PDO

--- ranking.php
+++ ranking.php
@@ -34,7 +34,8 @@ $variables['color_line1'] = $tkireg->color_line1;
 $variables['color_line2'] = $tkireg->color_line2;
 
 // Load required language variables for the ranking page.
-$langvars = Tki\Translate::load($pdo_db, $lang, array('main', 'ranking', 'common', 'global_includes', 'global_funcs', 'footer', 'teams'));
+$langvars = Tki\Translate::load($pdo_db, $lang,
+            array('main', 'ranking', 'common', 'global_includes', 'global_funcs', 'footer', 'teams'));
 
 // Get requested ranking order.
 // Detect if this variable exists, and filter it. Returns false if anything wasn't right.
@@ -72,83 +73,88 @@ switch ($sort)
 
 $variables['num_players'] = (int) 0;
 
-$rs = $db->SelectLimit("SELECT {$db->prefix}ships.ship_id, {$db->prefix}ships.email, {$db->prefix}ships.ip_address, {$db->prefix}ships.score, {$db->prefix}ships.character_name, {$db->prefix}ships.turns_used, {$db->prefix}ships.last_login,UNIX_TIMESTAMP({$db->prefix}ships.last_login) as online, {$db->prefix}ships.rating, {$db->prefix}teams.team_name, {$db->prefix}teams.admin AS team_admin, if ({$db->prefix}ships.turns_used<150,0,ROUND({$db->prefix}ships.score/{$db->prefix}ships.turns_used)) AS efficiency FROM {$db->prefix}ships LEFT JOIN {$db->prefix}teams ON {$db->prefix}ships.team = {$db->prefix}teams.id  WHERE ship_destroyed='N' and email NOT LIKE '%@xenobe' AND turns_used >0 ORDER BY $by", $tkireg->max_ranks);
-Tki\Db::LogDbErrors($pdo_db, $rs, __LINE__, __FILE__);
-if ($rs instanceof ADORecordSet)
+$sql = "SELECT {$db->prefix}ships.ship_id, {$db->prefix}ships.email, {$db->prefix}ships.ip_address, " .
+"{$db->prefix}ships.score, {$db->prefix}ships.character_name, {$db->prefix}ships.turns_used, " .
+"{$db->prefix}ships.last_login, UNIX_TIMESTAMP({$db->prefix}ships.last_login) as online, " .
+"{$db->prefix}ships.rating, {$db->prefix}teams.team_name, {$db->prefix}teams.admin AS team_admin, " .
+"if ({$db->prefix}ships.turns_used < 150, 0, ROUND({$db->prefix}ships.score/{$db->prefix}ships.turns_used)) " .
+"AS efficiency FROM {$db->prefix}ships LEFT JOIN {$db->prefix}teams ON " .
+"{$db->prefix}ships.team = {$db->prefix}teams.id WHERE ship_destroyed='N' and email NOT LIKE '%@xenobe' " .
+"AND turns_used > 0 ORDER BY :order_by LIMIT :limit";
+
+$stmt = $pdo_db->prepare($sql);
+$stmt->bindParam(':order_by', $by);
+$stmt->bindParam(':limit', $tkireg->max_ranks);
+$stmt->execute();
+$rankings = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$variables['num_players'] = (int) count($rankings);
+$player_list = array();
+$xx = 1;
+
+if ($rankings !== null && ($variables['num_players'] > 0))
 {
-    $variables['num_players'] = (int) $rs->RecordCount();
-
-    if ($variables['num_players'] > 0)
+    foreach ($rankings as $row)
     {
-        $player_list = array();
+        // Set the players rank number.
+        $row['rank'] = count($player_list) + 1;
 
-        while (!$rs->EOF)
+        // Calculate the players rating.
+        $rating = round(sqrt(abs($row['rating'])));
+        if (abs($row['rating']) != $row['rating'])
         {
-            $row = $rs->fields;
-
-            // Set the players rank number.
-            $row['rank'] = count($player_list) + 1;
-
-            // Calculate the players rating.
-            $rating = round(sqrt(abs($row['rating'])));
-            if (abs($row['rating']) != $row['rating'])
-            {
-                $rating = -1 * $rating;
-            }
-            $row['rating'] = $rating;
-
-            // Calculate the players online status.
-            $curtime = time();
-            $time = $row['online'];
-            $difftime = ($curtime - $time) / 60;
-            $temp_turns = $row['turns_used'];
-            if ($temp_turns <= 0)
-            {
-                $temp_turns = 1;
-            }
-
-            // Set the players online/offline status.
-            $row['online'] = (boolean) false;
-            if ($difftime <= 5)
-            {
-                $row['online'] = (boolean) true;
-            }
-
-            // Set the characters Insignia.
-            $row['insignia'] = Tki\Character::getInsignia($pdo_db, $row['email'], $langvars);
-
-            // This is just to show that we can set the type of player.
-            // like: banned, admin, player, npc etc.
-            if ($row['email'] == $tkireg->admin_mail || $row['team_admin'] === 'Y')
-            {
-                $row['type'] = 'admin';
-            }
-            else
-            {
-                $row['type'] = 'player';
-            }
-
-            // Check for banned players.
-            $ban_result = Tki\CheckBan::isBanned($pdo_db, $row);
-
-            if ($ban_result === false || (array_key_exists('ban_type', $ban_result) && $ban_result['ban_type'] === ID_WATCH))
-            {
-                $row['banned'] = (boolean) false;
-                $row['ban_info'] = null;
-            }
-            else
-            {
-                $row['banned'] = (boolean) true;
-                $row['ban_info'] = array('type' => $ban_result['ban_type'], 'public_info' => "Player banned/locked for the following:\n{$ban_result['public_info']}");
-            }
-
-            array_push($player_list, $row);
-
-            $rs->MoveNext();
+            $rating = -1 * $rating;
         }
-        $player_list['container'] = 'player';
-        $template->addVariables('players', $player_list);
+        $row['rating'] = $rating;
+
+        // Calculate the players online status.
+        $curtime = time();
+        $time = $row['online'];
+        $difftime = ($curtime - $time) / 60;
+        $temp_turns = $row['turns_used'];
+        if ($temp_turns <= 0)
+        {
+            $temp_turns = 1;
+        }
+
+        // Set the players online/offline status.
+        $row['online'] = (boolean) false;
+        if ($difftime <= 5)
+        {
+            $row['online'] = (boolean) true;
+        }
+
+        // Set the characters Insignia.
+        $row['insignia'] = Tki\Character::getInsignia($pdo_db, $row['email'], $langvars);
+
+        // This is just to show that we can set the type of player.
+        // like: banned, admin, player, npc etc.
+        if ($row['email'] == $tkireg->admin_mail || $row['team_admin'] === 'Y')
+        {
+            $row['type'] = 'admin';
+        }
+        else
+        {
+           $row['type'] = 'player';
+        }
+
+        // Check for banned players.
+        $ban_result = Tki\CheckBan::isBanned($pdo_db, $row);
+
+        if ($ban_result === false || (array_key_exists('ban_type', $ban_result) && $ban_result['ban_type'] === ID_WATCH))
+        {
+            $row['banned'] = (boolean) false;
+            $row['ban_info'] = null;
+        }
+        else
+        {
+            $row['banned'] = (boolean) true;
+            $row['ban_info'] = array('type' => $ban_result['ban_type'],
+                'public_info' => "Player banned/locked for the following:\n{$ban_result['public_info']}");
+        }
+
+        array_push($player_list, $row);
     }
+    $template->addVariables('players', $player_list);
 }
 
 if (empty ($_SESSION['username']))
@@ -163,11 +169,11 @@ else
 }
 
 Tki\Header::display($pdo_db, $lang, $template, $variables['title'], $variables['body_class']);
-
 $template->addVariables('variables', $variables);
 
 // Load required language variables for the ranking page.
-$langvars = Tki\Translate::load($pdo_db, $lang, array('main', 'ranking', 'common', 'global_includes', 'global_funcs', 'footer', 'teams', 'news'));
+$langvars = Tki\Translate::load($pdo_db, $lang,
+            array('main', 'ranking', 'common', 'global_includes', 'global_funcs', 'footer', 'teams', 'news'));
 
 // Modify the requires language variables here.
 $langvars['l_ranks_title'] = str_replace('[max_ranks]', $tkireg->max_ranks, $langvars['l_ranks_title']);
