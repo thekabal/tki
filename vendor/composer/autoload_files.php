dd3cf41d208a53084a62f27e8fc8de8468ce4501
The Kabal <thekabal@gmail.com>
2016-07-09 11:35:08 -0600

    Update to latest version of Swiftmailer

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -7,6 +7,6 @@ $baseDir = dirname($vendorDir);
 
 return array(
     '2cffec82183ee1cea088009cef9a6fc3' => $vendorDir . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
-    '2c102faa651ef8ea5874edb585946bce' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
     'bf9f5270ae66ac6fa0290b4bf47867b7' => $vendorDir . '/adodb/adodb-php/adodb.inc.php',
+    '2c102faa651ef8ea5874edb585946bce' => $vendorDir . '/swiftmailer/swiftmailer/lib/swift_required.php',
 );
