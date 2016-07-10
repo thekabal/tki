0b19136b70a16e01af1398ddff3b611dad0dd1fd
The Kabal <thekabal@gmail.com>
2016-07-10 14:01:20 -0600

    Fix minor errors for automated tests

--- tests/Test.php
+++ tests/Test.php
@@ -1,8 +1,27 @@
 <?php
+declare(strict_types=1);
+// The Kabal Invasion - A web-based 4X space game
+// Copyright © 2014 The Kabal Invasion development team, Ron Harwood, and the BNT development team
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// File: classes/Timer.php
 
 class Test extends PHPUnit_Framework_TestCase
 {
-    public function testOnePlusOne() {
+    public function testOnePlusOne()
+    {
         $this->assertEquals(1 + 1, 2);
     }
 }
