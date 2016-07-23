57981eccc74ec167b994976812cb73add26bc2f1
Patrick <thekabal@gmail.com>
2016-07-23 14:29:39 -0600

    Make variable assignment more explicit

--- classes/Translate.php
+++ classes/Translate.php
@@ -68,10 +68,10 @@ class Translate
                 $result->execute();
                 Db::logDbErrors($pdo_db, $query, __LINE__, __FILE__);
 
-                while (($row = $result->fetch()) !== false)
+/*                while (($row = $result->fetch()) !== false)
                 {
                     self::$langvars[$row['name']] = $row['value'];
-                }
+                }*/
             }
             return (Array) self::$langvars;
         }
