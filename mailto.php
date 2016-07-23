2e8bb1f72034b6f8f545e7c4d9756d9cdf6e0fc2
The Kabal <thekabal@gmail.com>
2016-07-23 14:19:44 -0600

    Make variable assignment more explicit

--- mailto.php
+++ mailto.php
@@ -21,10 +21,13 @@ require_once './common.php';
 
 Tki\Login::checkLogin($pdo_db, $lang, $tkireg, $template);
 
+$body_class = 'tki';
+$include_ckeditor = true;
+
 // Database driven language entries
 $langvars = Tki\Translate::load($pdo_db, $lang, array('mailto', 'common', 'global_includes', 'global_funcs', 'footer', 'planet_report'));
 $title = $langvars['l_sendm_title'];
-Tki\Header::display($pdo_db, $lang, $template, $title, $body_class = 'tki', $include_ckeditor = true);
+Tki\Header::display($pdo_db, $lang, $template, $title, $body_class, $include_ckeditor);
 
 // Filter to the FILTER_SANITIZE_STRING ruleset, because we need to allow spaces for names & subject (FILTER_SANITIZE_URL doesn't allow spaces)
 // $name, $to, and $subject are all sent both via post and get, so we have to do a filter input for each
