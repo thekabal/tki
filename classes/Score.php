a0a3098e33e67152a9dc763c0df2f22adeefe522
Patrick <thekabal@gmail.com>
2016-08-13 18:44:49 -0600

    Add setters and getters to Registry, and change code in other files to better facilitate search/replace on registry items

--- classes/Score.php
+++ classes/Score.php
@@ -30,41 +30,41 @@ class Score
         // $base_organics = $tkireg->base_organics;
 
         // These are all SQL Queries, so treat them like them.
-        $calc_hull              = "ROUND(POW($tkireg->upgrade_factor, hull))";
-        $calc_engines           = "ROUND(POW($tkireg->upgrade_factor, engines))";
-        $calc_power             = "ROUND(POW($tkireg->upgrade_factor, power))";
-        $calc_computer          = "ROUND(POW($tkireg->upgrade_factor, computer))";
-        $calc_sensors           = "ROUND(POW($tkireg->upgrade_factor, sensors))";
-        $calc_beams             = "ROUND(POW($tkireg->upgrade_factor, beams))";
-        $calc_torp_launchers    = "ROUND(POW($tkireg->upgrade_factor, torp_launchers))";
-        $calc_shields           = "ROUND(POW($tkireg->upgrade_factor, shields))";
-        $calc_armor             = "ROUND(POW($tkireg->upgrade_factor, armor))";
-        $calc_cloak             = "ROUND(POW($tkireg->upgrade_factor, cloak))";
-        $calc_levels            = "($calc_hull + $calc_engines + $calc_power + $calc_computer + $calc_sensors + $calc_beams + $calc_torp_launchers + $calc_shields + $calc_armor + $calc_cloak) * $tkireg->upgrade_cost";
+        $calc_hull              = "ROUND(POW(" . $tkireg->upgrade_factor . ", hull))";
+        $calc_engines           = "ROUND(POW(" . $tkireg->upgrade_factor . ", engines))";
+        $calc_power             = "ROUND(POW(" . $tkireg->upgrade_factor . ", power))";
+        $calc_computer          = "ROUND(POW(" . $tkireg->upgrade_factor . ", computer))";
+        $calc_sensors           = "ROUND(POW(" . $tkireg->upgrade_factor . ", sensors))";
+        $calc_beams             = "ROUND(POW(" . $tkireg->upgrade_factor . ", beams))";
+        $calc_torp_launchers    = "ROUND(POW(" . $tkireg->upgrade_factor . ", torp_launchers))";
+        $calc_shields           = "ROUND(POW(" . $tkireg->upgrade_factor . ", shields))";
+        $calc_armor             = "ROUND(POW(" . $tkireg->upgrade_factor . ", armor))";
+        $calc_cloak             = "ROUND(POW(" . $tkireg->upgrade_factor . ", cloak))";
+        $calc_levels            = "($calc_hull + $calc_engines + $calc_power + $calc_computer + $calc_sensors + $calc_beams + $calc_torp_launchers + $calc_shields + $calc_armor + $calc_cloak) * " . $tkireg->upgrade_cost;
 
-        $calc_torps             = "::prefix::ships.torps * $tkireg->torpedo_price";
-        $calc_armor_pts         = "armor_pts * $tkireg->armor_price";
-        $calc_ship_ore          = "ship_ore * $tkireg->ore_price";
-        $calc_ship_organics     = "ship_organics * $tkireg->organics_price";
-        $calc_ship_goods        = "ship_goods * $tkireg->goods_price";
-        $calc_ship_energy       = "ship_energy * $tkireg->energy_price";
-        $calc_ship_colonists    = "ship_colonists * $tkireg->colonist_price";
-        $calc_ship_fighters     = "ship_fighters * $tkireg->fighter_price";
+        $calc_torps             = "::prefix::ships.torps * " . $tkireg->torpedo_price;
+        $calc_armor_pts         = "armor_pts * " . $tkireg->armor_price;
+        $calc_ship_ore          = "ship_ore * " . $tkireg->ore_price;
+        $calc_ship_organics     = "ship_organics * " . $tkireg->organics_price;
+        $calc_ship_goods        = "ship_goods * " . $tkireg->goods_price;
+        $calc_ship_energy       = "ship_energy * " . $tkireg->energy_price;
+        $calc_ship_colonists    = "ship_colonists * " . $tkireg->colonist_price;
+        $calc_ship_fighters     = "ship_fighters * " . $tkireg->fighter_price;
         $calc_equip             = "$calc_torps + $calc_armor_pts + $calc_ship_ore + $calc_ship_organics + $calc_ship_goods + $calc_ship_energy + $calc_ship_colonists + $calc_ship_fighters";
 
-        $calc_dev_warpedit      = "dev_warpedit * $tkireg->dev_warpedit_price";
-        $calc_dev_genesis       = "dev_genesis * $tkireg->dev_genesis_price";
-        $calc_dev_beacon        = "dev_beacon * $tkireg->dev_beacon_price";
-        $calc_dev_emerwarp      = "dev_emerwarp * $tkireg->dev_emerwarp_price";
-        $calc_dev_escapepod     = "IF(dev_escapepod='Y', $tkireg->dev_escapepod_price, 0)";
-        $calc_dev_fuelscoop     = "IF(dev_fuelscoop='Y', $tkireg->dev_fuelscoop_price, 0)";
-        $calc_dev_lssd          = "IF(dev_lssd='Y', $tkireg->dev_lssd_price, 0)";
-        $calc_minedeflector     = "dev_minedeflector * $tkireg->dev_minedeflector_price";
+        $calc_dev_warpedit      = "dev_warpedit * " . $tkireg->dev_warpedit_price;
+        $calc_dev_genesis       = "dev_genesis * " . $tkireg->dev_genesis_price;
+        $calc_dev_beacon        = "dev_beacon * " . $tkireg->dev_beacon_price;
+        $calc_dev_emerwarp      = "dev_emerwarp * " . $tkireg->dev_emerwarp_price;
+        $calc_dev_escapepod     = "IF(dev_escapepod='Y', " . $tkireg->dev_escapepod_price . ", 0)";
+        $calc_dev_fuelscoop     = "IF(dev_fuelscoop='Y', " . $tkireg->dev_fuelscoop_price . ", 0)";
+        $calc_dev_lssd          = "IF(dev_lssd='Y', " . $tkireg->dev_lssd_price . ", 0)";
+        $calc_minedeflector     = "dev_minedeflector * " . $tkireg->dev_minedeflector_price;
         $calc_dev               = "$calc_dev_warpedit + $calc_dev_genesis + $calc_dev_beacon + $calc_dev_emerwarp + $calc_dev_escapepod + $calc_dev_fuelscoop + $calc_minedeflector + $calc_dev_lssd";
 
-        $calc_planet_goods      = "SUM(::prefix::planets.organics) * $tkireg->organics_price + SUM(::prefix::planets.ore) * $tkireg->ore_price + SUM(::prefix::planets.goods) * $tkireg->goods_price + SUM(::prefix::planets.energy) * $tkireg->energy_price";
-        $calc_planet_cols       = "SUM(::prefix::planets.colonists) * $tkireg->colonist_price";
-        $calc_planet_defense    = "SUM(::prefix::planets.fighters) * $tkireg->fighter_price + IF(::prefix::planets.base='Y', $tkireg->base_credits + SUM(::prefix::planets.torps) * $tkireg->torpedo_price, 0)";
+        $calc_planet_goods      = "SUM(::prefix::planets.organics) * " . $tkireg->organics_price . "+ SUM(::prefix::planets.ore) * " . $tkireg->ore_price . "+ SUM(::prefix::planets.goods) * " . $tkireg->goods_price . "+ SUM(::prefix::planets.energy) * " . $tkireg->energy_price;
+        $calc_planet_cols       = "SUM(::prefix::planets.colonists) * " . $tkireg->colonist_price;
+        $calc_planet_defense    = "SUM(::prefix::planets.fighters) * " . $tkireg->fighter_price . "+ IF(::prefix::planets.base='Y', " . $tkireg->base_credits . "+ SUM(::prefix::planets.torps) * " . $tkireg->torpedo_price . ", 0)";
         $calc_planet_credits    = "SUM(::prefix::planets.credits)";
 
         $sql = "SELECT IF(COUNT(*)>0, $calc_planet_goods + $calc_planet_cols + $calc_planet_defense + $calc_planet_credits, 0) AS planet_score " .
