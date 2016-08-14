b0bd26ca45cb09eecee395d0b6d257ea6a01be38
Patrick <thekabal@gmail.com>
2016-08-13 21:35:17 -0600

    Add return type annotation to clarify possible mixed returns (unlikely, but I haven't audited the code to confirm it only returns ints|float)

--- classes/Ports.php
+++ classes/Ports.php
@@ -115,6 +115,9 @@ class Ports
         return $delta_cost;
     }
 
+    /*
+     * @return mixed
+     */
     // Here is the trade fonction to strip out some "spaghetti code". The function saves about 60 lines of code, I hope it will be
     // easier to modify/add something in this part.
     public static function trade($price, $delta, $max, $limit, $factor, $port_type, $origin, Array $price_array, Array $sectorinfo)
