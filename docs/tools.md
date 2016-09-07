0d5a352bd53b6455e7478a3d2b269db6046c5b60
The Kabal <thekabal@gmail.com>
2016-09-06 23:13:35 -0600

    Tools -> vendor/bin completed

--- docs/tools.md
+++ docs/tools.md
@@ -15,19 +15,19 @@ found in the `/vendor/bin` directory.
   least once. We have not resolved all errors it identifies yet, but are making
   progress towards doing so. We use it like this:
 
-  `php vendor/bin/phpdcd --exclude=templates --exclude=vendor`
+  `php vendor/bin/phpdcd . --exclude=templates --exclude=vendor`
 
 - [`phpcs`](https://github.com/squizlabs/PHP_CodeSniffer) is PHP Code Sniffer.
   PHP_CodeSniffer tokenizes PHP, JavaScript and CSS files and detects violations
   of a defined set of coding standards. We use it like this:
 
-  `php vendor/bin/phpcs --standard=phpcs.xml . --ignore=templates,vendor,config,languages`
+  `php vendor/bin/phpcs --standard=vendor/bin/phpcs.xml . --ignore=templates,vendor,config,languages`
 
 - [`phpmd`](https://phpmd.org/) is PHP Mess Detector. It takes a given PHP
   source code base and looks for several potential problems within that source.
   We use it like this:
 
-  `php vendor/bin/phpmd . text phpmd.xml --exclude vendor/,templates/`
+  `php vendor/bin/phpmd . text vendor/bin/phpmd.xml --exclude vendor/,templates/`
 
 - [`php-cs-fixer`](http://cs.sensiolabs.org/) strives to automatically correct
   code for specific items in the PSR standard. We do not currently use it, but
---
+++ vendor/friendsofphp/php-cs-fixer/src/Console/Command/SelfUpdateCommand.php
@@ -0,0 +1,101 @@
+<?php
+
+/*
+ * This file is part of PHP CS Fixer.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *     Dariusz Rumiński <dariusz.ruminski@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace PhpCsFixer\Console\Command;
+
+use PhpCsFixer\ToolInfo;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Igor Wiedler <igor@wiedler.ch>
+ * @author Stephane PY <py.stephane1@gmail.com>
+ * @author Grégoire Pineau <lyrixx@lyrixx.info>
+ *
+ * @internal
+ */
+final class SelfUpdateCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('self-update')
+            ->setAliases(array('selfupdate'))
+            ->setDescription('Update php-cs-fixer.phar to the latest stable version.')
+            ->setHelp(<<<'EOT'
+The <info>%command.name%</info> command replace your php-cs-fixer.phar by the
+latest version from cs.sensiolabs.org.
+
+<info>php php-cs-fixer.phar %command.name%</info>
+
+EOT
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!ToolInfo::isInstalledAsPhar()) {
+            $output->writeln('<error>Self-update is available only for PHAR version.</error>');
+
+            return 1;
+        }
+
+        if (false !== $remoteVersion = @file_get_contents('http://get.sensiolabs.org/php-cs-fixer.version')) {
+            if ($this->getApplication()->getVersion() === $remoteVersion) {
+                $output->writeln('<info>php-cs-fixer is already up to date.</info>');
+
+                return;
+            }
+        }
+
+        $remoteFilename = 'http://get.sensiolabs.org/php-cs-fixer.phar';
+        $localFilename = $_SERVER['argv'][0];
+        $tempFilename = basename($localFilename, '.phar').'-tmp.phar';
+
+        if (false === @file_get_contents($remoteFilename)) {
+            $output->writeln('<error>Unable to download new versions from the server.</error>');
+
+            return 1;
+        }
+
+        try {
+            copy($remoteFilename, $tempFilename);
+            chmod($tempFilename, 0777 & ~umask());
+
+            // test the phar validity
+            $phar = new \Phar($tempFilename);
+            // free the variable to unlock the file
+            unset($phar);
+            rename($tempFilename, $localFilename);
+
+            $output->writeln('<info>php-cs-fixer updated.</info>');
+        } catch (\Exception $e) {
+            if (!$e instanceof \UnexpectedValueException && !$e instanceof \PharException) {
+                throw $e;
+            }
+
+            unlink($tempFilename);
+            $output->writeln(sprintf('<error>The download is corrupt (%s).</error>', $e->getMessage()));
+            $output->writeln('<error>Please re-run the self-update command to try again.</error>');
+
+            return 1;
+        }
+    }
+}
