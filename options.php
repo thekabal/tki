eba873d114690957196c355026430a94cf0d3965
The Kabal <thekabal@gmail.com>
2017-01-29 17:51:29 -0700

    Fix function call

--- options.php
+++ options.php
@@ -56,7 +56,7 @@ echo "<tr>\n";
 echo "<td>" . $langvars['l_opt_select'] . "</td><td><select name=newlang>\n";
 
 $avail_langs = Tki\Languages::listAvailable($pdo_db, $lang);
-foreach($avail_langs as $language_list_item_name => $language_list_item)
+foreach ($avail_langs as $language_list_item_name => $language_list_item)
 {
     if ($language_list_item_name == $playerinfo['lang'])
     {
