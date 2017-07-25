15c235834ddb92ea0ab02440e6e81c3a8eff9eb0
thekabal <thekabal@gmail.com>
2017-07-25 00:43:21 -0600

    Minor change in usage

--- vendor/bin/tki-code-check.sh
+++ vendor/bin/tki-code-check.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-/usr/bin/php /var/www/html/tki/vendor/bin/phpmd . text /var/www/html/tki/vendor/bin/phpmd.xml --exclude vendor/,templates
+/usr/bin/php /var/www/html/tki/vendor/bin/phpmd . text /var/www/html/tki/vendor/bin/phpmd.xml --exclude vendor/,templates/
 /usr/bin/php /var/www/html/tki/vendor/bin/phpcs -q --standard=/var/www/html/tki/vendor/bin/phpcs.xml . --ignore=templates,vendor
 /usr/bin/php /var/www/html/tki/vendor/bin/phpstan analyze --no-progress --no-ansi -l 5 -c /var/www/html/tki/vendor/bin/phpstan.neon -vvv .
 
