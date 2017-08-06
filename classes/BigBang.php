2aaedaddcf293e1374bd15c71e012d054c4940cf
thekabal <thekabal@gmail.com>
2017-08-06 14:00:12 -0600

    Replace short variable names with longer/accurate names

--- classes/BigBang.php
+++ classes/BigBang.php
@@ -23,7 +23,7 @@ class BigBang
 {
     public function findStep(string $current_file) : array
     {
-        $i = 0;
+        $num_files = 0;
 
         // Setup $bigbang_files as an array type.
         // We add a null value as index 0 due to we need to start from index 1.
@@ -39,8 +39,8 @@ class BigBang
             // If it is a PHP file, add it to the list of accepted make galaxy files
             if ($file_info->isFile() && $file_info->getExtension() == 'php')
             {
-                $i++; // Increment a counter, so we know how many files there are to choose from
-                $filelist[$i] = $file_info->getFilename(); // The actual file name
+                $num_files++; // Increment a counter, so we know how many files there are to choose from
+                $filelist[$num_files] = $file_info->getFilename(); // The actual file name
             }
         }
 
@@ -56,7 +56,7 @@ class BigBang
         // Now remove the unwanted array.
         unset($filelist);
 
-        $bigbang_info['steps'] = $i;
+        $bigbang_info['steps'] = $num_files;
         if ($current_file === '')
         {
             // If current file is set to null string, just return the search from 0.
@@ -68,16 +68,16 @@ class BigBang
             $bigbang_info['current_step'] = array_search(basename($current_file), $bigbang_files);
         }
 
-        if (($bigbang_info['current_step'] + 1) > $i)
+        if (($bigbang_info['current_step'] + 1) > $num_files)
         {
-            $j = $i;
+            $new_current_file = $num_files;
         }
         else
         {
-            $j = $bigbang_info['current_step'] + 1;
+            $new_current_file = $bigbang_info['current_step'] + 1;
         }
 
-        $bigbang_info['next_step'] = array_search($bigbang_files[$j], $bigbang_files);
+        $bigbang_info['next_step'] = array_search($bigbang_files[$new_current_file], $bigbang_files);
         $bigbang_info['files'] = $bigbang_files;
 
         return $bigbang_info;
