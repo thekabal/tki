47d25a384c945e7bf0163f2f325bf66c44886df3
thekabal <thekabal@gmail.com>
2018-04-15 15:43:34 +0000

    Simplify logic

--- classes/TraderouteBuildNew.php
+++ classes/TraderouteBuildNew.php
@@ -397,12 +397,6 @@ class TraderouteBuildNew
             </form>
             ";
 
-        echo "<div style='text-align:left;'>\n";
-        \Tki\Text::gotoMain($pdo_db, $lang);
-        echo "</div>\n";
-
-        $footer = new \Tki\Footer();
-        $footer->display($pdo_db, $lang, $tkireg, $template);
-        die();
+        \Tki\TraderouteDie::die($pdo_db, $lang, $tkireg, $template, null);
     }
 }
