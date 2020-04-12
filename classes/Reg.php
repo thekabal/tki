f10393f3c47fc41e8173da0631215e431f351b80
The Kabal <thekabal@gmail.com>
2020-04-12 12:06:54 -0400

    Nope, it was in the right place ;-)

--- classes/Reg.php
+++ classes/Reg.php
@@ -19,6 +19,8 @@
 
 namespace Tki;
 
+class Reg
+{
     /**
      * @property mixed account_creation_closed
      * @property mixed admin_mail
@@ -172,8 +174,7 @@ namespace Tki;
      * @property mixed kabal_unemployment
      *
      */
-class Reg
-{
+
     /** @var Array **/
     protected $vars = array();
 
