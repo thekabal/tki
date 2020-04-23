8995db02f33bf74f231292395bba5f291c6f856d
The Kabal <thekabal@gmail.com>
2020-04-23 12:09:04 -0400

    Switch header style to be a proper PHPDoc

--- classes/KabalToPlanet.php
+++ classes/KabalToPlanet.php
@@ -1,21 +1,26 @@
 <?php declare(strict_types = 1);
-// The Kabal Invasion - A web-based 4X space game
-// Copyright © 2014 The Kabal Invasion development team, Ron Harwood, and the BNT development team
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as
-//  published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// File: classes/KabalToPlanet.php
+/**
+ * classes/KabalToPlanet.php from The Kabal Invasion.
+ * The Kabal Invasion is a Free & Opensource (FOSS), web-based 4X space/strategy game.
+ *
+ * @copyright 2020 The Kabal Invasion development team, Ron Harwood, and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 namespace Tki;
 
--- scheduler/sched_ports.php
+++ scheduler/sched_ports.php
@@ -1,24 +1,28 @@
 <?php declare(strict_types = 1);
-// The Kabal Invasion - A web-based 4X space game
-// Copyright © 2014 The Kabal Invasion development team, Ron Harwood, and the BNT development team
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Affero General Public License as
-//  published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Affero General Public License for more details.
-//
-//  You should have received a copy of the GNU Affero General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// File: sched_ports.php
-//
-// FUTURE: PDO, Debugging, Output
+/**
+ * scheduler/sched_ports.php from The Kabal Invasion.
+ * The Kabal Invasion is a Free & Opensource (FOSS), web-based 4X space/strategy game.
+ *
+ * @copyright 2020 The Kabal Invasion development team, Ron Harwood, and the BNT development team
+ *
+ * @license GNU AGPL version 3.0 or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
+// FUTURE: PDO, Debugging, Output
 // Database driven language entries
 $langvars = Tki\Translate::load($pdo_db, $lang, array('scheduler'));
 
