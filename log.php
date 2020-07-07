68686517f8f5c63aea5ea971ef57d448122bd08a
The Kabal <thekabal@gmail.com>
2020-07-07 11:57:24 -0400

    Oops, typo/consistency in SQL call

--- log.php
+++ log.php
@@ -117,7 +117,7 @@ if (empty($startdate))
     $startdate = date("Y-m-d");
 }
 
-$sql = "SELECT * FROM ::prefix::logs WHERE ship_id=:ship_id AND time LIKE :start_date ORDER BY time DESC, type DESC";
+$sql = "SELECT * FROM ::prefix::logs WHERE ship_id=:ship_id AND time LIKE ':start_date%' ORDER BY time DESC, type DESC";
 $stmt = $pdo_db->prepare($sql);
 $stmt->bindParam(':ship_id', $playerinfo['ship_id'], PDO::PARAM_INT);
 $stmt->bindValue(':start_date', $startdate . '%');
