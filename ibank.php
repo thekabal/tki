017269aedd304b1ad868ffbb6034c781606a3bfe
The Kabal <thekabal@gmail.com>
2021-01-28 19:19:13 -0500

    Missing a parameter

--- ibank.php
+++ ibank.php
@@ -52,7 +52,7 @@ echo '<table style="width:550px; height:300px;" border="0px">';
 
 if (!$tkireg->allow_ibank)
 {
-    Tki\Ibank::ibankError($pdo_db, $langvars, $langvars['l_ibank_malfunction'], "main.php", $lang, $tkireg, $template);
+    Tki\Ibank::ibankError($pdo_db, $langvars, $langvars['l_ibank_malfunction'], "main.php", $lang, $tkireg, $tkitimer, $template);
 }
 
 // Detect if this variable exists, and filter it. Returns false if anything wasn't right.
